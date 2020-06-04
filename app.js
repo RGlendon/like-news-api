4b23897eae7bcc845e0833b55bd4ead306fa6e2c
RGlendon <Rold.Glendon@gmail.com>
2020-06-04 23:23:56 +0300

    add custom error messages for celebrate and central err handlers

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 require('dotenv').config();
 
 const { createUser, login } = require('./controllers/users');
 const CustomError = require('./helpers/custom-error');
+const CelebrateErrHandler = require('./middlewares/celebrate-err-handler');
+const CentralErrHandler = require('./middlewares/central-err-handler');
 const { validatePassword } = require('./helpers/validations');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -40,6 +42,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().custom(validatePassword),
   }),
 }), createUser);
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -58,24 +61,8 @@ app.use('/', (req, res, next) => {
 
 app.use(errorLogger);
 
-app.use(errors());
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  if (err.name === 'MongoError') {
-    return res.status(409)
-      .send({ message: `пользователь с ${Object.keys(err.keyValue)}: ${Object.values(err.keyValue)} уже существует` });
-  }
-  if (err.message.includes('validation failed')) {
-    return res.status(400)
-      .send({ message: `${err.message.replace(/.*validation failed: /, '')}` });
-  }
-  // res.send(err);
-  res.status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-
-  return next();
-});
+app.use(CelebrateErrHandler);
+app.use(CentralErrHandler);
 
 
 app.listen(PORT, () => {
